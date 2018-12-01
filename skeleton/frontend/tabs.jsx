fe9b5afab839d512e01c8d422497ffda97e68c85
pzengpzeng <pzeng.pzeng@gmail.com>
2018-11-30 17:01:46 -0800

    Work on tabs selections

--- skeleton/frontend/tabs.jsx
+++ skeleton/frontend/tabs.jsx
@@ -1,23 +1,46 @@
 import React from 'react';
 
+// const Header = (props) => {
+//   const selectedClass =
+//     //
+//
+//   render() {
+//     return (
+//       <li
+//       className={selectStatus}></li>
+//     )
+//   }
+// };
+
 class Tabs extends React.Component {
   constructor(props) {
     super(props);
     this.state = { selected: 0 };
+    // this.makeSelected = this.makeSelected.bind(this);
   }
 
-  makeSelected() {
+  makeSelected(e) {
     this.setState({
-      selected: 1
+      selected: e.target.id
     });
+
+    for (let i = 0; i < this.props.tabs.length; i++) {
+      if (e.target.id === this.state.selected) {
+        e.target.className = "selected";
+      } else {
+        e.target.classList.remove("selected");
+        e.target.className = "hidden";
+      }
+    }
   }
 
   render() {
     return(
       <ul className="tabs-ul">
-        {this.props.tabs.map(tab => (
+        <h1>{this.state.selected}</h1>
+        {this.props.tabs.map((tab, index) => (
           <li>
-            <h1 onClick={()=> this.makeSelected()}>{tab.title}</h1>
+            <h1 id={index} onClick={(e)=> this.makeSelected(e)}>{tab.title}</h1>
             <article>{tab.content}</article>
           </li>)
           )}
--- skeleton/root.jsx
+++ skeleton/root.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Clock from './frontend/clock';
 import Tabs from './frontend/tabs';
+import Weather from './frontend/weather';
 // import Articles from './frontend/articles';
 
 const articles = [
   {title: "RaccoonFartBox", content: "Rear faltulence"},
   {title: "I love coffee", content: "Coffee is great, it keeps me from being late"},
-  {title: "The person behind me", content: "Is annoying"}
+  {title: "The person behind me", content: "Is great"}
 ];
 
 const Root = () => (
